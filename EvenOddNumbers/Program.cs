9969b05d5b3d49cfc0fb062a8bb358460dc54a70
badrsaeed <55393290+badrsaeed@users.noreply.github.com>
2023-02-04 08:17:30 +0200

    initial Commit

---
+++ EvenOddNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Print(numbers, "this is Numbers");
+            Print(numbers.Where(x => IsEven(x)), "Even No: ");
+            Print(numbers.Where(x => IsOdd(x)), "Odd No: ");
+            Console.ReadKey();
+        }
+        private static void Print(IEnumerable<int> numbers, string title)
+        {
+            Console.WriteLine();
+            Console.Write($"{title}: [");
+            foreach (int n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+            Console.Write($" ]");
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+        static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
